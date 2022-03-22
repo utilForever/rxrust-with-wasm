54ea528e7cf8d7a242b6e1259df77f20f47b0e04
Chris Ohk <utilforever@gmail.com>
2022-03-22 14:36:01 +0900

    feat: Add macro 'log' and replace code to use 'alert' with it

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+extern crate web_sys;
+
 use rxrust::prelude::*;
 use wasm_bindgen::prelude::*;
+
+// A macro to provide 'println!(..)'-style syntax for 'console.log' logging.
+#[allow(unused_macros)]
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into())
+    }
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -9,6 +19,6 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     let o = observable::from_iter(0..10);
-    o.clone().subscribe(|_| alert("Hello"));
-    o.clone().subscribe(|_| alert("rust-wasm!"));
+    o.clone().subscribe(|_| log!("Hello"));
+    o.clone().subscribe(|_| log!("rust-wasm!"));
 }
